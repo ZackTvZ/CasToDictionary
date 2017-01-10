5176fda264b659248305ac5203a66ac739f89d89
zack <zacktvz@hotmail.my>
2017-01-10 18:27:07 +0800

    init

---
+++ CasToDictionary/CasToDictionary.h
@@ -0,0 +1,19 @@
+//
+//  CasToDictionary.h
+//  CasToDictionary
+//
+//  Created by ZackTvZ on 10/01/2017.
+//  Copyright © 2017 ZackTvZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CasToDictionary.
+FOUNDATION_EXPORT double CasToDictionaryVersionNumber;
+
+//! Project version string for CasToDictionary.
+FOUNDATION_EXPORT const unsigned char CasToDictionaryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CasToDictionary/PublicHeader.h>
+
+
